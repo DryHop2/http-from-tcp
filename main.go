36d230bdd05e8377665772289bbcf99dd858ac83
DryHop2 <tyler.savon.smith@gmail.com>
2025-05-27 05:10:04 -0600

    feat: read a file 8 bytes at a time and print to stdout

--- main.go
+++ main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	buffer := make([]byte, 8)
+
+	for {
+		bytesRead, err := file.Read(buffer)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		fmt.Printf("read: %s\n", buffer[:bytesRead])
+	}
+
 }
